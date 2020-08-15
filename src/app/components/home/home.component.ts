949acbf08ed7dd64a4b36fd92216409ab5f31e25
Pancham Kumar Gupt <panchamkumargupt@Panchams-MacBook-Pro.local>
2020-08-15 20:47:07 +0530

    map

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder, NativeGeocoderOptions, NativeGeocoderResult } from '@ionic-native/native-geocoder/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +9,100 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+// Readable Address
+address: string;
+lat
+lng
+// Location coordinates
+latitude: number;
+longitude: number;
+accuracy: number;
 
-  constructor(private router: Router) { }
+// Geocoder configuration
+geoencoderOptions: NativeGeocoderOptions = {
+  useLocale: true,
+  maxResults: 5
+};
+  constructor(private router: Router, private geolocation: Geolocation , private nativeGeocoder: NativeGeocoder) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    // this.currentLocation()
+    this.getGeolocation()
+   }
 
+
+  //  //Get current coordinates of device
+   getGeolocation() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+
+      this.latitude = resp.coords.latitude;
+      this.longitude = resp.coords.longitude;
+      this.accuracy = resp.coords.accuracy;
+console.log( this.latitude,this.longitude)
+      this.getGeoencoder(resp.coords.latitude, resp.coords.longitude);
+
+    }).catch((error) => {
+      alert('Error getting location' + JSON.stringify(error));
+    });
+  }
+
+
+  //geocoder method to fetch address from coordinates passed as arguments
+  getGeoencoder(latitude, longitude) {
+    this.nativeGeocoder.reverseGeocode(latitude, longitude, this.geoencoderOptions)
+      .then((result: NativeGeocoderResult[]) => {
+        // console.log(result)
+        this.address = this.generateAddress(result[0]);
+      })
+      .catch((error: any) => {
+        console.log('add')
+        alert('Error getting location' + JSON.stringify(error));
+      });
+  }
+
+
+
+
+  // //Return Comma saperated address
+  generateAddress(addressObj) {
+    
+    let obj = [];
+    let address = "";
+    for (let key in addressObj) {
+      obj.push(addressObj[key]);
+    }
+    obj.reverse();
+    for (let val in obj) {
+      if (obj[val].length)
+        address += obj[val] + ', ';
+    }
+
+    return address.slice(0, -2);
+  }
+
+  // currentLocation(){
+
+  
+  // this.geolocation.getCurrentPosition(
+  //   {maximumAge: 1000, timeout: 5000,
+  //    enableHighAccuracy: true }
+  //   ).then((resp) => {
+  //         // resp.coords.latitude
+  //         // resp.coords.longitude
+  //         //alert("r succ"+resp.coords.latitude)
+  //         alert(JSON.stringify( resp.coords));
+    
+  //         this.lat=resp.coords.latitude
+  //         this.lng=resp.coords.longitude
+  //         },er=>{
+  //           //alert("error getting location")
+  //           alert('Can not retrieve Location')
+  //         }).catch((error) => {
+  //         //alert('Error getting location'+JSON.stringify(error));
+  //         alert('Error getting location - '+JSON.stringify(error))
+  //         });
+
+  //       }
   save() {
     this.router.navigate(['/map'])
   }
--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentInit, ViewChild,ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
+import { makeBindingParser } from '@angular/compiler';
+
+declare var google:any;
 
 @Component({
   selector: 'app-map',
@@ -7,13 +10,87 @@ import { Router } from '@angular/router';
   styleUrls: ['./map.component.scss'],
 })
 export class MapComponent implements OnInit {
+map:any;
+@ViewChild('map',{read:ElementRef,static:false}) mapRef:ElementRef;
+
+infoWindows:any=[];
+markers:any=[
+  {
+    title:"National Art Gallery",
+    latitude:'-17.824991',
+    longitude:'31.049295'
+  }
+];
 
   constructor(private router:Router) { }
 
   ngOnInit() {}
 
+ionViewDidEnter(){
+  this.showMap();
+}
+
+
+
+showMap(){
+  const location=new google.maps.LatLng(-17.824858,31.053028);
+  const options={
+    center:location,
+    zoom:15,
+    disableDefaultUI:true
+  }
+  this.map=new google.maps.Map(this.mapRef.nativeElement,options)
+  console.log(this.map)
+  this.addMarkersToMap(this.markers)
+}
+
+
+// Add Marker
+
+addMarkersToMap(markers){
+    for(let marker of markers){
+      let position=new google.maps.LatLng(marker.latitude,marker.longitude);
+      let mapMarker= new google.maps.Marker({
+        position:position,
+        title:marker.title,
+        latitude:marker.latitude,
+        longitude:marker.longitude
+      });
+      mapMarker.setMap(this.map);
+      this.addInfoWindowToMarker(mapMarker);
+    }
+  }
+
+
+  addInfoWindowToMarker(marker){
+let infoWindowContent='<div id="content">'+
+                       '<h2 id="firstHeading" class="firstHeading">' +marker.title +'</h2>'+
+                       '<P>Latitude: ' +marker.latitude +'</p>'+
+                       '<P>Longitude: ' +marker.longitude +'</p>'+
+                       '</div>  '
+
+  let infoWindow= new google.maps.infoWindow({
+    content:infoWindowContent
+  });
+
+  marker.addListener('click',()=>{
+    console.log("click")
+    this.closeAllInfoWindows();
+    infoWindow.open(this.map,marker);
+  });
+  this.infoWindows.push(infoWindow)
+  }
+
+
+  closeAllInfoWindows(){
+    for(let window of this.infoWindows){
+      window.close();
+    }
+  }
 
   addReport(){
 this.router.navigate(['/click-photo'])
   }
+
+
 }
